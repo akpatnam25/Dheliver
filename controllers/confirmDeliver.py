152076d62596ce3a825ab9208ba83ee2085b9605
Aravind Patnam <akpatnam@shyams-imac.T-mobile.com>
2018-05-27 15:04:11 -0700

    deliver.html added and mostly finished

---
+++ controllers/confirmDeliver.py
@@ -0,0 +1,8 @@
+def index():
+    return dict()
+
+def data():
+    if not session.m or len(session.m)==10: session.m=[]
+    if request.vars.q: session.m.append(request.vars.q)
+    session.m.sort()
+    return TABLE(*[TR(v) for v in session.m]).xml()
