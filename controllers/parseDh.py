cd4e55755a416b9b51c0811e19102966b9de98e0
Jonathan Kung <jkung2314@gmail.com>
2018-06-07 13:33:13 -0700

    Replace parseDh.py

---
+++ controllers/parseDh.py
@@ -0,0 +1,295 @@
+import parseDh
+def getCowellBreakFast():
+    f = open('../Dheliver/resources/cowellBreakfast.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getCowellLunch():
+    f = open('../Dheliver/resources/cowellLunch.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+
+def getCowellDinner():
+    f = open('../Dheliver/resources/cowellDinner.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getCowellLateNight():
+    f = open('../Dheliver/resources/cowellLateNight.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getMerillBreakfast():
+    f = open('../Dheliver/resources/merillBreakfast.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getMerillLunch():
+    f = open('../Dheliver/resources/merillLunch.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+def getMerillDinner():
+    f = open('../Dheliver/resources/merillDinner.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+def getMerillLateNight():
+    f = open('../Dheliver/resources/merillLateNight.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+def getPorterBreakfast():
+    f = open('../Dheliver/resources/porterBreakfast.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+def getPorterLunch():
+    f = open('../Dheliver/resources/porterLunch.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+def getPorterDinner():
+    f = open('../Dheliver/resources/porterDinner.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getPorterLateNight():
+    f = open('../Dheliver/resources/porterLateNight.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getRccBreakfast():
+    f = open('../Dheliver/resources/rccBreakfast.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getRccLunch():
+    f = open('../Dheliver/resources/rccLunch.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getRccDinner():
+    f = open('../Dheliver/resources/rccDinner.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getRccLateNight():
+    f = open('../Dheliver/resources/rccLateNight.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getTenBreakfast():
+    f = open('../Dheliver/resources/tenBreakfast.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getTenLunch():
+    f = open('../Dheliver/resources/tenLunch.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getTenDinner():
+    f = open('../Dheliver/resources/tenDinner.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
+
+
+def getTenLateNight():
+    f = open('../Dheliver/resources/tenLateNight.txt','r')
+    message = f.read()
+    strMessage = str(message)
+    finalStr = ""
+    for i in range(len(strMessage)-3):
+        if strMessage[i].isalpha() :
+            finalStr += strMessage[i]
+        else:
+            finalStr += " "
+    words = finalStr.split("    ")
+    words[0] = words[0].replace(" ", "")
+    print words
